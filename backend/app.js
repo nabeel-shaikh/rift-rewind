191a0cd0746c1a530664c24de82765400a6e6306
nabeel-shaikh <shaikhnab23@gmail.com>
2025-10-23 01:40:52 -0400

    inital backend setup with express server

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,17 @@
 import express from "express";
-const app = express();
+import cors from "cors";
+import dotenv from "dotenv";
 
-app.get("/", (req, res) => {
-  res.send("Rift Rewind backend working!");
+dotenv.config(); //load environment variables from .env file
+const app = express();
+app.use(cors()); // allow front end requests
+app.use(express.json()); // parse incoming JSON data/requests
+
+
+app.get("/api/test", (req, res) => {
+  res.send("Rift Rewind backend working");
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+//start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=> console.log("Server running on port "+PORT));
